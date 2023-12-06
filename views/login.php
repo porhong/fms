ea9f1bd2f36413bd6b527b50e1204b774c29f277
porhong <poehongkeat@gmail.com>
2023-12-07 00:47:19 +0700

    V1.0.1[Dev] - Update UI (Continue)

--- views/login.php
+++ views/login.php
@@ -23,10 +23,7 @@ if (isset($_GET['success']) && $_GET['success'] == 1) {
     <title>Login</title>
     <link rel="stylesheet" href="../sources/style.css" />
     <!-- Link Bootstrap5 -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 
 <body class="set-bg-to-white">
@@ -35,7 +32,7 @@ if (isset($_GET['success']) && $_GET['success'] == 1) {
 
 
         <!-- Login Container -->
-        <div class="row border bg-white shadow disable-shadow-sm box-area auth-card">
+        <div class="row border disable-shadow-sm bg-white box-area auth-card">
 
 
             <!-- Left Box -->
@@ -76,6 +73,9 @@ if (isset($_GET['success']) && $_GET['success'] == 1) {
         </div>
     </div>
     </div>
+<!-- Bootstrap JS and dependencies (optional) -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
