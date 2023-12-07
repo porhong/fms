a3b65d3f7c4f06faf632c51fdb1f0da18bc11d11
porhong <poehongkeat@gmail.com>
2023-12-07 11:00:30 +0700

    V1.0.1[Dev] - Updated Register and login UI

--- views/login.php
+++ views/login.php
@@ -23,7 +23,7 @@ if (isset($_GET['success']) && $_GET['success'] == 1) {
     <title>Login</title>
     <link rel="stylesheet" href="../sources/style.css" />
     <!-- Link Bootstrap5 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body class="set-bg-to-white">
@@ -32,7 +32,7 @@ if (isset($_GET['success']) && $_GET['success'] == 1) {
 
 
         <!-- Login Container -->
-        <div class="row border disable-shadow-sm bg-white box-area auth-card">
+        <div class="row bg-white box-area auth-card">
 
 
             <!-- Left Box -->
@@ -43,16 +43,18 @@ if (isset($_GET['success']) && $_GET['success'] == 1) {
 
             </div>
             <!-- Right Box -->
-            <div class="col-md-6 rigtt-box">
+            <div class="col-md-6 right-box">
                 <div class="header-text">
                     <p class="auth-title-1">Hello, Welcome</p>
                     <p class="auth-title-2">Please login to continue</p>
                 </div>
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Username or Email">
+                <div class="form-floating input mb-3">
+                    <input id="txt_username_email" type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Username or Email">
+                    <label for="txt_username_email">Username or Email</label>
                 </div>
-                <div class="input-group mb-1">
-                    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password">
+                <div class="form-floating input mb-1">
+                    <input id="txt_password" type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password">
+                    <label for="txt_password">Password</label>
                 </div>
                 <div class="input-group mb-3 d-flex justify-content-between">
                     <div class="form-check">
@@ -66,16 +68,15 @@ if (isset($_GET['success']) && $_GET['success'] == 1) {
                 <div class="input-group mb-3">
                     <button class="btn btn-lg btn-primary w-100 fs-6">Login</button>
                 </div>
-                <div class="row pb-3">  
+                <div class="row pb-3">
                     <small>Don't have account? <a href="../views/register.php">Sign Up</a></small>
                 </div>
             </div>
         </div>
     </div>
     </div>
-<!-- Bootstrap JS and dependencies (optional) -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <!-- Bootstrap JS and dependencies (optional) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- views/register.php
+++ views/register.php
@@ -10,9 +10,10 @@ register();
 
 <head>
     <title>Login</title>
-    <link rel="stylesheet" href="../sources/style.css" />
+    <link rel=" stylesheet" href="../sources/style.css" />
     <!-- Link Bootstrap5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <!-- Link JS -->
     <script type="text/javascript" src="../controlers/JS/User_JS_Controler.js"></script>
 </head>
@@ -23,11 +24,14 @@ register();
 
 
         <!-- Login Container -->
-        <div class="row border bg-white shadow box-area auth-card">
+        <div class="row bg-white box-area auth-card">
 
 
             <!-- Left Box -->
-            <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box">
+            <div class=" position-relative col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box">
+                <div class=" icon_back position-absolute top-0 start-0">
+                    <a href="../views/login.php"><i style="font-size: 2.2rem;" class=" bi bi-arrow-left-circle"></i></a>
+                </div>
                 <div class="featured-image">
                     <img src="../sources/img/logo.svg" class="img-fluid" style="width: 250px;">
                 </div>
@@ -39,31 +43,37 @@ register();
                     <p class="auth-title-1">Register</p>
                     <p class="auth-title-2">Please fill the information below</p>
                 </div>
-                <div id="block_txt_username" class="input mb-3">
+                <div id="block_txt_username" class="form-floating form-floating input mb-3">
                     <input oninput="verifyInput('txt_username','Username is required')" onblur="checkFromDB('txt_username','username')" type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Username" name="txt_username" id="txt_username" required>
+                    <label for="txt_username">Username</label>
                 </div>
-                <div id="block_txt_password" class="input mb-3">
+                <div id="block_txt_password" class="form-floating input mb-3">
                     <input oninput="verifyPassword()" type="password" class="form-control form-control-lg bg-light fs-6 border" placeholder="Password" name="txt_password" id="txt_password" required>
+                    <label for="txt_password">Password</label>
                 </div>
-                <div id="block_txt_confirm_password" class="input mb-3">
+                <div id="block_txt_confirm_password" class="form-floating input mb-3">
                     <input oninput="verifyPassword()" type="password" class="form-control form-control-lg bg-light fs-6 border" placeholder="Confirm Password" name="txt_confirm_password" id="txt_confirm_password" required>
+                    <label for="txt_confirm_password">Confirm Password</label>
                 </div>
-                <div class=" row mb-3">
-                    <div class="col" id="block_txt_first_name">
-                        <input oninput="verifyInput('txt_first_name','First name is required')" type="text" class="form-control form-control-lg bg-light fs-6" placeholder="First Name" name="txt_first_name" id="txt_first_name" required>
+                <div class="row mb-2">
+                    <div class="col form-floating input" id="block_txt_first_name">
+                        <input oninput="verifyInput('txt_first_name','First name is required')" type="text" class="form-control form-label form-control-lg bg-light fs-6" placeholder="First Name" name="txt_first_name" id="txt_first_name" required>
+                        <label class="ms-3" for="txt_first_name">First Name</label>
                     </div>
-                    <div class="col" id="block_txt_last_name">
+                    <div class="col form-floating input" id="block_txt_last_name">
                         <input oninput="verifyInput('txt_last_name','Last name is required')" type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Last name" name="txt_last_name" id="txt_last_name" required>
+                        <label class="ms-3" for="txt_last_name">Last Name</label>
                     </div>
                 </div>
-                <div id="block_txt_email" class="input mb-3">
+                <div id="block_txt_email" class="form-floating input mb-3">
                     <input oninput="verifyMail('txt_email')" onblur="checkFromDB('txt_email','email')" type="email" class="form-control form-control-lg bg-light fs-6" placeholder="Email" name="txt_email" id="txt_email" required>
+                    <label for="txt_email">Email</label>
                 </div>
                 <div class="form-check mb-4">
                     <input onclick="formVrify(true)" type="checkbox" class="form-check-input" id="formAgree">
                     <label for="formAgree" class="form-check-label text-secondary"><small>I have read and agree to badbot-chayluy Terms of service and Privacy Policy.</small></label>
                 </div>
-                <div class="input mb-3">
+                <div class="form-floating input mb-3">
                     <button id="btn_submit" type="submit" name="submit" value="submit" class="btn btn-lg btn-primary w-100 fs-6" disabled>Register</button>
                 </div>
             </form>
