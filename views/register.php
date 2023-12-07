4e4536d56066de461862bf6542a6f87f90a41c45
porhong <poehongkeat@gmail.com>
2023-12-07 17:29:33 +0700

    Fixed View port

--- views/register.php
+++ views/register.php
@@ -10,6 +10,7 @@ register();
 
 <head>
     <title>Register</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel=" stylesheet" href="../sources/style.css" />
     <!-- Link Bootstrap5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
